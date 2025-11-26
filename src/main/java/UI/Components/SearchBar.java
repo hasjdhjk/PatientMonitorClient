058fe48cff0d01da46f1793606f08df4ceb6f48a
hasjdhjk <h7182005@163.com>
2025-11-26 22:39:15 +0000

    f

--- src/main/java/UI/Components/SearchBar.java
+++ src/main/java/UI/Components/SearchBar.java
@@ -1,6 +1,7 @@
 package UI.Components;
 
 import UI.Components.Tiles.BaseTile;
+import Utilities.ImageLoader;
 
 import javax.swing.*;
 import java.awt.*;
@@ -11,9 +12,14 @@ public class SearchBar extends BaseTile {
         super(400, 65, 40);
         setLayout(new BorderLayout());
 
+        // search field
         JTextField searchField = new JTextField();
-        searchField.setBorder(null);
+        searchField.setBorder(BorderFactory.createEmptyBorder(0, 10, 0, 0));
         searchField.setOpaque(false);
+
+        // icon
+        JLabel searchLabel = new JLabel(ImageLoader.loadImage("icon_search", "Icons", 25));
+        add(searchLabel, BorderLayout.WEST);
 
         add(searchField, BorderLayout.CENTER);
         setBorder(BorderFactory.createEmptyBorder(0, 30, 0, 30)); // make sure text inside teh border
